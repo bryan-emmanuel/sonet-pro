4d2a934be35fd48933701fc8253edb0719759cad
piusvelte <piusvelte@gmail.com>
2012-01-26 13:01:43 -0500

    sunc current

---
+++ src/com/piusvelte/sonetpro/ManageAccounts.java
@@ -0,0 +1,241 @@
+/*
+ * Sonet - Android Social Networking Widget
+ * Copyright (C) 2009 Bryan Emmanuel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Bryan Emmanuel piusvelte@gmail.com
+ */
+package com.piusvelte.sonetpro;
+
+import static com.piusvelte.sonetpro.Sonet.ACTION_REFRESH;
+import static com.piusvelte.sonetpro.Sonet.RESULT_REFRESH;
+import static com.piusvelte.sonetpro.Sonet.ACCOUNTS_QUERY;
+import static com.piusvelte.sonetpro.Sonet.RSS;
+import static com.piusvelte.sonetpro.Sonet.SMS;
+import static com.piusvelte.sonetpro.SonetProvider.TABLE_WIDGET_ACCOUNTS;
+import static com.piusvelte.sonetpro.SonetProvider.TABLE_ACCOUNTS;
+
+import com.piusvelte.sonetpro.Sonet.Accounts;
+import com.piusvelte.sonetpro.Sonet.Notifications;
+import com.piusvelte.sonetpro.Sonet.Status_links;
+import com.piusvelte.sonetpro.Sonet.Statuses;
+import com.piusvelte.sonetpro.Sonet.Widget_accounts;
+import com.piusvelte.sonetpro.Sonet.Widgets;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.appwidget.AppWidgetManager;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+public class ManageAccounts extends ListActivity implements OnClickListener, DialogInterface.OnClickListener {
+	private static final int REAUTH_ID = Menu.FIRST;
+	private static final int SETTINGS_ID = Menu.FIRST + 1;
+	private static final int ENABLE_ID = Menu.FIRST + 2;
+	private static final int DELETE_ID = Menu.FIRST + 3;
+	private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+	private boolean mAddingAccount,
+	mUpdateWidget = false;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.accounts);
+
+		Intent intent = getIntent();
+		if (intent != null) {
+			Bundle extras = intent.getExtras();
+			if (extras != null) {
+				mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+			}
+			// if called from widget, the id is set in the action, as pendingintents must have a unique action
+			else if ((intent.getAction() != null) && (!intent.getAction().equals(ACTION_REFRESH)) && (!intent.getAction().equals(Intent.ACTION_VIEW))) {
+				mAppWidgetId = Integer.parseInt(intent.getAction());
+			}
+		}
+
+		Intent resultValue = new Intent();
+		resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+		setResult(RESULT_OK, resultValue);
+
+		registerForContextMenu(getListView());
+		((Button) findViewById(R.id.default_widget_settings)).setOnClickListener(this);
+		((Button) findViewById(R.id.button_add_account)).setOnClickListener(this);
+		((Button) findViewById(R.id.save)).setOnClickListener(this);
+	}
+
+	private final SimpleCursorAdapter.ViewBinder mViewBinder = new SimpleCursorAdapter.ViewBinder() {
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if (columnIndex == cursor.getColumnIndex(Widget_accounts.WIDGET)) {
+				((CheckBox) view).setChecked(cursor.getInt(columnIndex) == 1);
+				return true;
+			} else return false;
+		}
+	};
+
+	@Override
+	protected void onListItemClick(ListView list, final View view, int position, final long id) {
+		super.onListItemClick(list, view, position, id);
+		final CharSequence[] items = {getString(R.string.re_authenticate), getString(R.string.account_settings), getString(((CheckBox) view.findViewById(R.id.isenabled)).isChecked() ? R.string.disable : R.string.enable)};
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setItems(items, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				which++; //fix indexing
+				switch (which) {
+				case REAUTH_ID:
+					// need the account id if reauthenticating
+					Cursor c = getContentResolver().query(Accounts.CONTENT_URI, new String[]{Accounts.SERVICE}, Accounts._ID + "=?", new String[]{Long.toString(id)}, null);
+					if (c.moveToFirst()) {
+						int service = c.getInt(0);
+						if ((service != SMS) && (service != RSS)) {
+							mAddingAccount = true;
+							startActivityForResult(new Intent(ManageAccounts.this, OAuthLogin.class).putExtra(Accounts.SERVICE, service).putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId).putExtra(Sonet.EXTRA_ACCOUNT_ID, id), RESULT_REFRESH);
+						}
+					}
+					c.close();
+					break;
+				case SETTINGS_ID:
+					mAddingAccount = true;
+					startActivityForResult(new Intent(ManageAccounts.this, AccountSettings.class).putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId).putExtra(Sonet.EXTRA_ACCOUNT_ID, id), RESULT_REFRESH);
+					break;
+				case ENABLE_ID:
+					if (((CheckBox) view.findViewById(R.id.isenabled)).isChecked()) {
+						// disable the account, remove settings and statuses
+						getContentResolver().delete(Widgets.CONTENT_URI, Widgets.ACCOUNT + "=? and " + Widgets.WIDGET + "=?", new String[]{Long.toString(id), Integer.toString(mAppWidgetId)});
+						getContentResolver().delete(Widget_accounts.CONTENT_URI, Widget_accounts.ACCOUNT + "=? and " + Widget_accounts.WIDGET + "=?", new String[]{Long.toString(id), Integer.toString(mAppWidgetId)});
+						getContentResolver().delete(Statuses.CONTENT_URI, Statuses.ACCOUNT + "=? and " + Statuses.WIDGET + "=?", new String[]{Long.toString(id), Integer.toString(mAppWidgetId)});
+						listAccounts();
+					} else {
+						// enable the account
+						ContentValues values = new ContentValues();
+						values.put(Widget_accounts.ACCOUNT, id);
+						values.put(Widget_accounts.WIDGET, mAppWidgetId);
+						ManageAccounts.this.getContentResolver().insert(Widget_accounts.CONTENT_URI, values);
+						listAccounts();
+					}
+					mUpdateWidget = true;
+					listAccounts();
+					break;
+				}
+				dialog.cancel();
+			}
+		}).show();
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, view, menuInfo);
+		menu.add(0, DELETE_ID, 0, R.string.delete_account);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		if (item.getItemId() == DELETE_ID) {
+			mUpdateWidget = true;
+			String accountId = Long.toString(((AdapterContextMenuInfo) item.getMenuInfo()).id);
+			getContentResolver().delete(Accounts.CONTENT_URI, Accounts._ID + "=?", new String[]{accountId});
+			// need to delete the statuses and settings for all accounts
+			getContentResolver().delete(Widgets.CONTENT_URI, Widgets.ACCOUNT + "=?", new String[]{accountId});
+			Cursor statuses = getContentResolver().query(Statuses.CONTENT_URI, new String[]{Statuses._ID}, Statuses.ACCOUNT + "=?", new String[]{accountId}, null);
+			if (statuses.moveToFirst()) {
+				while (!statuses.isAfterLast()) {
+					getContentResolver().delete(Status_links.CONTENT_URI, Status_links.STATUS_ID + "=?", new String[]{Long.toString(statuses.getLong(0))});
+					statuses.moveToNext();
+				}
+			}
+			statuses.close();
+			getContentResolver().delete(Statuses.CONTENT_URI, Statuses.ACCOUNT + "=?", new String[]{accountId});
+			getContentResolver().delete(Widget_accounts.CONTENT_URI, Widget_accounts.ACCOUNT + "=?", new String[]{accountId});
+			getContentResolver().delete(Notifications.CONTENT_URI, Notifications.ACCOUNT + "=?", new String[]{accountId});
+		}
+		return super.onContextItemSelected(item);
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_add_account:
+			// add a new account
+			String[] services = getResources().getStringArray(R.array.service_entries);
+			(new AlertDialog.Builder(this))
+			.setItems(services, this)
+			.show();
+			break;
+		case R.id.default_widget_settings:
+			mAddingAccount = true;
+			startActivityForResult(new Intent(this, Settings.class).putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId), RESULT_REFRESH);
+			break;
+		case R.id.save:
+			finish();
+			break;
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		listAccounts();
+		mAddingAccount = false;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (!mAddingAccount && mUpdateWidget) {
+			startService(new Intent(this, SonetService.class).setAction(ACTION_REFRESH).putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, new int[]{mAppWidgetId}));
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if ((requestCode == RESULT_REFRESH) && (resultCode == RESULT_OK)) {
+			mUpdateWidget = true;
+		}
+	}
+
+	private void listAccounts() {
+		// list all accounts, checking the checkbox if they are enabled for this widget
+		// prepend service name to username
+		Cursor c = this.managedQuery(Accounts.CONTENT_URI, new String[]{Accounts._ID, ACCOUNTS_QUERY, "case when (select " + Widget_accounts.WIDGET + " from " + TABLE_WIDGET_ACCOUNTS + " where " + Widget_accounts.WIDGET + "=" + mAppWidgetId + " and " + Widget_accounts.ACCOUNT + "=" + TABLE_ACCOUNTS + "." + Accounts._ID + " limit 1) is null then 0 else 1 end as " + Widget_accounts.WIDGET}, null, null, null);
+		SimpleCursorAdapter sca = new SimpleCursorAdapter(ManageAccounts.this, R.layout.accounts_row, c, new String[] {Accounts.USERNAME, Widget_accounts.WIDGET}, new int[] {R.id.account_username, R.id.isenabled});
+		sca.setViewBinder(mViewBinder);
+		setListAdapter(sca);
+	}
+
+
+	public void onClick(DialogInterface dialog, int which) {
+		mAddingAccount = true;
+		startActivityForResult(new Intent(this, OAuthLogin.class).putExtra(Accounts.SERVICE, which).putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId).putExtra(Sonet.EXTRA_ACCOUNT_ID, Sonet.INVALID_ACCOUNT_ID), RESULT_REFRESH);
+		dialog.cancel();
+	}
+
+}
